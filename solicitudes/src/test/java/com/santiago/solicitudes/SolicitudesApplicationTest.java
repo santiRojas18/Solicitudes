d7816c37a0eff211d23b82916a94f940ef92ed0c
santiRojas18 <sanrojas@poligran.edu.co>
2026-01-13 13:51:17 -0500

    config: configurar H2 para producción y tests; arreglar test contextLoads

---
+++ solicitudes/src/test/java/com/santiago/solicitudes/SolicitudesApplicationTest.java
@@ -0,0 +1,14 @@
+package com.santiago.solicitudes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SolicitudesApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
